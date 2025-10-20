455ce720dd216901d455ca8c00382fa04d7b7dae
MateusLeopoldo <165858153+MateusLeopoldo@users.noreply.github.com>
2025-10-20 14:47:40 -0300

    FIx

--- loja/views.py
+++ loja/views.py
@@ -16,8 +16,144 @@ class ListarLojas(ListView):
         queryset = super().get_queryset().order_by('nome')
         query = self.request.GET.get('q')
         if query:
+<<<<<<< Updated upstream
             queryset = queryset.filter(
                 Q(nome__icontains=query) |
                 Q(endereco__icontains=query)
             )
         return queryset
+=======
+            queryset = queryset.filter(Q(nome__icontains=query) | Q(endereco__icontains=query))
+        
+        favoritas = self.request.GET.get('favoritas')
+        if favoritas:
+            queryset = queryset.filter(favoritada_por=self.request.user)
+        
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            favoritas_ids = LojaFavorita.objects.filter(usuario=self.request.user).values_list('loja_id', flat=True)
+            context['favoritas_usuario'] = list(favoritas_ids)
+        else:
+            context['favoritas_usuario'] = []
+        return context
+
+
+
+class CriarLoja(LoginRequiredMixin, CreateView):
+    model = Loja
+    form_class = FormularioLoja
+    template_name = 'loja/novo.html'
+    success_url = reverse_lazy('loja:loja_list')
+
+    def get_form(self, form_class=None):
+        """
+        Adiciona classes CSS aos campos do formulário para estilização com Bootstrap.
+        """
+        form = super().get_form(form_class)
+        for field_name, field in form.fields.items():
+            widget = field.widget
+            if isinstance(widget, Select):
+                widget.attrs.update({'class': 'form-select'})
+            elif isinstance(widget, CheckboxInput):
+                 widget.attrs.update({'class': 'form-check-input'})
+            elif isinstance(widget, (TextInput, Textarea, NumberInput, FileInput, EmailInput, TimeInput, URLInput)):
+                widget.attrs.update({'class': 'form-control'})
+        return form
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Cadastrar Nova Loja'
+        return context
+
+class LojaDetailView(LoginRequiredMixin, DetailView):
+    """
+    View para exibir os detalhes de uma loja e seus produtos, com filtros.
+    """
+    model = Loja
+    template_name = 'loja/loja_detail.html'
+    login_url = reverse_lazy('login')
+    context_object_name = 'loja'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loja = self.get_object()
+        
+        # Pega os parâmetros de filtro da requisição GET
+        search_query = self.request.GET.get('q', '')
+        categoria_query = self.request.GET.get('categoria', '')
+        animal_query = self.request.GET.get('animal', '')
+        preco_min_query = self.request.GET.get('preco_min', '')
+        preco_max_query = self.request.GET.get('preco_max', '')
+
+        # Começa com todos os produtos disponíveis da loja
+        produtos = Produto.objects.filter(loja=loja, disponivel=True)
+        
+        # Aplica os filtros se eles existirem
+        if search_query:
+            produtos = produtos.filter(nome__icontains=search_query)
+        if categoria_query:
+            produtos = produtos.filter(categoria__id=categoria_query)
+        if animal_query:
+            produtos = produtos.filter(animal_destino=animal_query)
+        if preco_min_query:
+            produtos = produtos.filter(preco__gte=preco_min_query)
+        if preco_max_query:
+            produtos = produtos.filter(preco__lte=preco_max_query)
+
+        # Adiciona os produtos filtrados e as opções de filtro ao contexto
+        context['produtos'] = produtos.distinct()
+        context['categorias'] = Categoria.objects.all()
+        context['animal_choices'] = ANIMAL_CHOICES
+        context['porte_choices'] = PORTE_CHOICES
+        context['idade_choices'] = IDADE_CHOICES
+
+        context['avaliacoes'] = loja.avaliacoes.select_related('usuario').order_by('-criado_em')
+        user = self.request.user
+        if user.is_authenticated:
+            context['ja_avaliou'] = loja.avaliacoes.filter(usuario=user).exists()
+        else:
+            context['ja_avaliou'] = False
+
+        if user.is_authenticated:
+            context['favoritada'] = LojaFavorita.objects.filter(loja=loja, usuario=user).exists()
+        else:
+            context['favoritada'] = False
+
+        return context
+    
+    
+@login_required
+def avaliar_loja(request, loja_id):
+    loja = get_object_or_404(Loja, id=loja_id)
+
+    if request.method == 'POST':
+        nota = int(request.POST.get('nota', 0))
+        comentario = request.POST.get('comentario', '').strip()
+
+        Avaliacao.objects.update_or_create(
+            usuario=request.user,
+            loja=loja,
+            defaults={'nota': nota, 'comentario': comentario}
+        )
+
+        loja.atualizar_media()
+        return redirect('loja:loja_detail', pk=loja.id)
+
+    return redirect('loja:loja_detail', pk=loja.id)
+
+@login_required
+def favoritar_loja(request, loja_id):
+    loja = Loja.objects.get(pk=loja_id)
+    if request.user in loja.favoritada_por.all():
+        loja.favoritada_por.remove(request.user)
+        favoritado = False
+    else:
+        loja.favoritada_por.add(request.user)
+        favoritado = True
+
+    total_favoritos = loja.favoritada_por.count()
+    return JsonResponse({'favoritado': favoritado, 'total_favoritos': total_favoritos})
+>>>>>>> Stashed changes
